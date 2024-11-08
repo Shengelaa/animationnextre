d0b6a8f3a4e565392bb0dde806f7e158a822dcd9
OrionGift3 <168460082+Shengelaa@users.noreply.github.com>
2024-11-08 19:59:50 +0400

    core/structure

---
+++ app/constants/card.ts
@@ -0,0 +1,14 @@
+export type CardType = {
+  title: string;
+  id: number;
+  image: string;
+};
+
+export const cards: CardType[] = [
+  { title: "Title 1", id: 1, image: "./assets/images/image1.png" },
+  { title: "Title 2", id: 2, image: "./assets/images/image2.png" },
+  { title: "Title 3", id: 3, image: "./assets/images/image3.png" },
+  { title: "Title 4", id: 4, image: "./assets/images/image4.png" },
+  { title: "Title 5", id: 5, image: "./assets/images/image5.png" },
+  { title: "Title 6", id: 6, image: "./assets/images/image6.png" },
+];
